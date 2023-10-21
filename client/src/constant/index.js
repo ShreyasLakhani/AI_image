02f3bd3beae8fb38445f850f7391460ffced62b7
Shreyas <shreyaslakhani25@gmail.com>
2023-10-22 01:11:40 +0530

    first commit

---
+++ client/src/constant/index.js
@@ -0,0 +1,19 @@
+export const surpriseMePrompts = [
+  "A serene sunset over a tranquil lake",
+  "A bustling cityscape at night",
+  "A futuristic city in the year 3000",
+  "A medieval castle on a hill",
+  "A dense and mysterious rainforest",
+  "A snow-covered mountain range",
+  "An astronaut floating in space",
+  "A pirate ship sailing on the open sea",
+  "A dragon soaring above the clouds",
+  "An ancient temple hidden in the jungle",
+  "A robot playing a musical instrument",
+  "A group of penguins on an iceberg",
+  "A mermaid sitting on a rock in the ocean",
+  "A knight fighting a fire-breathing dragon",
+  "An alien landscape with multiple moons in the sky"
+]
+
+  
